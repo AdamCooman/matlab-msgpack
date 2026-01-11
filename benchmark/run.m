b1641bdd5b9f9fc68fd1175a37a1efe76a2573ff
Adam Cooman <adam.cooman@imec.be>
2026-01-11 12:29:40 +0100

    added benchmark

---
+++ benchmark/run.m
@@ -0,0 +1,50 @@
+clear variables
+% close all
+clc
+
+addpath("..");
+profile on
+
+types = ["double" "single" "uint8" "uint16" "uint32" "uint64" "int8" "int16" "int32" "int64"];
+number_of_elements = 2.^(1:2:16);
+for type = types
+    bytes_per_element = 8/numel(typecast(zeros(1,8,"uint8"),type));
+    for ii = 1 : numel(number_of_elements)
+        disp(type+" "+number_of_elements(ii))
+        data = randi(255,1,bytes_per_element*number_of_elements(ii),"uint8");
+        data = typecast(data,type);
+        pack = msgpack.dump(data);
+        res(ii).(type+"_dump_time") = timeit(@()msgpack.dump(data),1);
+        res(ii).(type+"_parse_time") = timeit(@()msgpack.parse(pack),1);
+    end
+end
+
+profile off
+profile viewer
+
+
+figure(1)
+tiledlayout(1,2)
+
+nexttile(1)
+for type = types
+    loglog(number_of_elements,[res.(type+"_dump_time")],DisplayName=type)
+    hold on
+end
+grid on
+legend show
+title("dump")
+ax = gca;
+ax.LineStyleOrder = ["-","-+","-o"];
+
+nexttile(2)
+for type = types
+    loglog(number_of_elements,[res.(type+"_parse_time")],DisplayName=type)
+    hold on
+end
+grid on
+legend show
+title("parse")
+ax = gca;
+ax.LineStyleOrder = ["-","-+","-o"];
+
